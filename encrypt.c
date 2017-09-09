fdbc210bad4a80d8ff7be4392a26533e91f34727
Seungwoo Lee <outtoin@kaist.ac.kr>
2017-09-10 00:24:03 +0900

    init

---
+++ encrypt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+
+}
---
+++ start.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "sys/types.h"
+#include "sys/socket.h"
+#include "netinet/in.h"
+
+//#define PORT 4000
+//#define BUF_LEN 1024
+
+void main(int argc, char *argv[])
+{
+  //int s, n;
+  //char *haddr;
+  //struct sockaddr_in server_addr;
+  //char buf[BUF_LEN+1];
+
+  if(argc == 2)
+}
